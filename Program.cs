a3c931486b54f74ecaee3483fae8f35ba75e89ac
Moloko Sehata <moloko4@live.com>
2023-10-10 10:57:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            Console.WriteLine("While Loop.");
+
+            while (index <+ 5) 
+            {
+                Console.WriteLine(index);
+                index ++;
+            }
+
+            Console.WriteLine("Do Loop. ");
+            int secondIndex = 0;
+
+            do
+            {                
+                Console.WriteLine(secondIndex);
+                secondIndex++;
+            } 
+            while (secondIndex <= 5);
+
+
+            Console.WriteLine("For Loop.");
+
+            for (int i = 0; i <= 5; i++) 
+            { 
+                Console.Write(i + ", ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
